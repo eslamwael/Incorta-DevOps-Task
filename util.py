0ab97f05fe8fa667fbd88f85b54fd389e9ed0f02
Eslam Wael <35847720+eslamwael@users.noreply.github.com>
2020-12-07 23:31:02 +0200

    Update util.py

--- util.py
+++ util.py
@@ -9,7 +9,7 @@ cpu_csvRow = [dt.datetime.now(),psutil.cpu_percent(interval=1, percpu=False) * 1
 cpu_csv = "/opt/CPU.csv"
 
 # Appending results in the csv file
-with open(cpu_csv, "a", newline='') as fp:
+with open(cpu_csv, "a") as fp:
     wr = csv.writer(fp, dialect='excel')
     wr.writerow(cpu_csvRow)
 
@@ -22,7 +22,7 @@ mem_csvRow = [dt.datetime.now(),mem_free_per]
 mem_csv = "/opt/MEM.csv"
 
 # Appending results in the csv file
-with open(mem_csv, "a", newline='') as fp:
+with open(mem_csv, "a") as fp:
     wr = csv.writer(fp, dialect='excel')
     wr.writerow(mem_csvRow)
 
@@ -35,6 +35,6 @@ disk_csvRow = [dt.datetime.now(),disk_free_per]
 disk_csv = "/opt/DISK.csv"
 
 # Appending results in the csv file
-with open(disk_csv, "a", newline='') as fp:
+with open(disk_csv, "a") as fp:
     wr = csv.writer(fp, dialect='excel')
     wr.writerow(disk_csvRow)
